f8fbe6e79a0b2251f710b76fe001e56566f681e4
Jhareign <74128037+Jhareign@users.noreply.github.com>
2023-02-02 17:19:45 +0800

    upgrade the website to a much much much better one. first commit : )

--- b.cmd
+++ b.cmd
@@ -2,6 +2,7 @@ git add .
 git commit -m %1
 git remote add origin https://github.com/great-anaphylaxis/great-anaphylaxis-website.git
 git remote -v
+git pull
 git push origin master
 
 firebase deploy
